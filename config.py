3e580592764ec24b9374df05074558c54e60f1cf
Carlos Silva Garcia <carlos.silva@elevenyellow.com>
2016-08-19 11:33:50 +0800

    update route icons

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 from os import path
 from inspect import getfile, currentframe
 
-sudoPassword = 'you password'
+sudoPassword = 'your password'
 base_dir = path.dirname(path.abspath(getfile(currentframe())))
-icon_path = '%s/%s' %(base_dir, 'icons')
+
+#options - icons or icons2
+icons = "icons2"
+icon_path = '%s/%s' %(base_dir, icons)
 
 #block function keys startup
 auto_block = True
