bbcd43348f57f492d6d26cbe4cc2cc015ca15d21
Carlos Silva Garcia <carlos.silva@elevenyellow.com>
2016-08-20 11:43:16 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+from os import system
+from shutil import copytree
+from config import base_dir
+import errno
+
+#udev permisions
+def create_file():
+    file_name = "99-keyboard.rules"
+    path = "/etc/udev/rules.d/"
+
+    print 'keyboards detected: '
+    command = "ls /dev/hidraw*"
+    system(command)
+
+    input_text = "hidraw number (default value *): "
+    value_input = raw_input(input_text)
+
+    if value_input is None or value_input is "":
+        hidraw = "*"
+    else:
+        hidraw = value_input
+
+    print "create rule udev %s" % file_name
+    file = open(path + file_name, 'w+')
+    text = 'KERNEL=="hidraw%s", SUBSYSTEM=="hidraw", ATTRS{address}=="*", MODE="0666"' % hidraw
+    file.write(text)
+    file.write('\n')
+    file.close()
+
+    dir_dev = "/dev/hidraw%s" % hidraw
+    print "change permision in %s to 0666" % dir_dev
+
+    command = "chmod 0666 %s" % dir_dev
+    system(command)
+
+    return
+
+#copi files
+def copy_files():
+    dest = '/opt/keyboard-applet/'
+
+    input_text = "change path (default value %s): " % dest
+    value_input = raw_input(input_text)
+
+    if value_input is None or value_input is "":
+        path = dest
+    else:
+        path = value_input
+
+    print "create folder keyboard-applet in %s" % path
+
+    try:
+        copytree(base_dir, path)
+    except OSError as exc:
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else:
+            raise
+
+    print "add python %sapplet.py in startup applications" % path
+
+    return
+
+create_file()
+copy_files()
+print 'final setup.'
